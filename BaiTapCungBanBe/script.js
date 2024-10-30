fc7dd1e47f390897ce2155245fd9f017c349d4a3
tranduyfong <tranphong86958@gmail.com>
2024-10-31 01:19:43 +0700

    Baitapvenha

---
+++ BaiTapCungBanBe/script.js
@@ -0,0 +1,12 @@
+function watchVideo1() {
+    document.getElementById("video-center").innerHTML = document.getElementById("video1").innerHTML;
+}
+function watchVideo2() {
+    document.getElementById("video-center").innerHTML = document.getElementById("video2").innerHTML;
+}
+function watchVideo3() {
+    document.getElementById("video-center").innerHTML = document.getElementById("video3").innerHTML;
+}
+function watchVideo4() {
+    document.getElementById("video-center").innerHTML = document.getElementById("video4").innerHTML;
+}
